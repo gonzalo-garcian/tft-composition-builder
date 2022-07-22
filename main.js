a747547115999f9ec74df27d96c0e383ef3dfe98
Gonzalo <gonzalo.garcian@e-campus.uab.cat>
2022-07-23 00:19:58 +0200

    Implemented: drag and drop images to the grid

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log("TFT Composition Builder")
+function onDragStart(event) {
+    event
+      .dataTransfer
+      .setData('text/plain', event.target.id);
+  
+    event
+      .currentTarget
+      .style
+      .backgroundColor = 'yellow';
+
+    console.log('DRAG WORKS!')
+  }
+
+  function onDragOver(event) {
+    event.preventDefault();
+  }
+
+  function onDrop(event) {
+
+    const id = event
+      .dataTransfer
+      .getData('text');
+
+      const draggableElement = document.getElementById(id);
+      draggableElement.classList.add('item-hex');
+      const dropzone = event.target;
+      dropzone.appendChild(draggableElement);
+
+      event
+    .dataTransfer
+    .clearData();
+  }
+
+  /* TODO: onDrag shape the image as a hexagon */

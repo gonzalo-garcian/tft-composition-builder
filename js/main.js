b3b0f3c60ac89d1911023facaeaf8d61295bc8de
Gonzalo <gonzalo.garcian@e-campus.uab.cat>
2022-07-24 23:26:03 +0200

    Created: synergies mockup

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 var globalCounter = 0;
 var synergies = [];
-
+var data = {
+  "Aatrox": {
+    name: "Aatrox",
+    synergies: ["Guild, Astral"]
+  }
+}
 function onDragStart(event) {
     event
       .dataTransfer
@@ -16,10 +21,20 @@ function onDragStart(event) {
       const id = event
       .dataTransfer
       .getData('text');
+
       const dropzone = event.target;
       if(dropzone.classList[0] == "hex" && !dropzone.hasChildNodes()){
         var draggableElement = document.getElementById(id);
         if(draggableElement.parentElement.id == 'pieces'){
+
+          /* TODO: function to control the synergies */
+          synergies.push(data[draggableElement.id]["synergies"])
+          synergies.forEach(element => {
+            const li = document.createElement("li");
+            li.innerHTML = "element";
+            document.getElementById("synergies").appendChild(li);
+          });
+          /*---------------------------------------- */
           draggableElement = document.getElementById(id).cloneNode(true);
           draggableElement.id = "item-cloned" + globalCounter;
           globalCounter++;
@@ -34,10 +49,10 @@ function onDragStart(event) {
     
   }
 
-  /*TODO: Look up if there is a better way to select the father*/
   function onContextMenu(event){
     event.preventDefault();
     if(event.target.id != "grid" && event.target.parentElement.id != "grid"){
       event.target.remove();
+      synergies.pop()
     }
   }
